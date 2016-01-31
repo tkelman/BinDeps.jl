a96cb96958b02b36254a37faff5341a06572378b
Tony Kelman <tony@kelman.net>
2016-01-30 22:32:09 -0800

    and another

--- src/dependencies.jl
+++ src/dependencies.jl
@@ -814,8 +814,8 @@ function satisfy!(dep::LibraryDependency, methods = defaults)
                     run(lower(generate_steps(ddep,dp,dopts)))
                 end
             end
-            @show generate_steps(dep,p,dopts)
-            @show lower(generate_steps(dep,p,dopts))
+            @show generate_steps(dep,p,opts)
+            @show lower(generate_steps(dep,p,opts))
             run(lower(generate_steps(dep,p,opts)))
             @show issatisfied(dep)
             !issatisfied(dep) && error("Provider $method failed to satisfy dependency $(dep.name)")
